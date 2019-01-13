072f4bc7104eb5c50a454dc746163f95a62b52c6
Ilija Hadzic <ilijahadzic@gmail.com>
2019-01-13 12:05:42 -0500

    support different view angles and spans for X and Y axes
    Signed-off-by: Ilija Hadzic <ilijahadzic@gmail.com>

--- project_points.m
+++ project_points.m
@@ -1,17 +1,20 @@
-function pp=project_points(cal_dist, cal_span, view_angle, distance, ...
+function pp=project_points(cal_dist, cal_span_x, cal_span_y, ...
+                           view_angle_x, view_angle_y, distance, ...
                            use_meters, do_plot)
 
-if nargin <= 4
+if nargin <= 6
   use_meters = false
 end
 reference_points = get_reference_points(use_meters);
-normalized_reference_points = reference_points / cal_span;
+normalized_reference_points =  [ ...
+    reference_points(:, 1)' / cal_span_x; ...
+    reference_points(:, 2)' / cal_span_y ]';
 delta_d = distance - cal_dist;
-
-fp = frustum_projection(cal_span, view_angle, delta_d);
-pp = normalized_reference_points .* fp;
-
-if nargin > 5
+fpx = frustum_projection(cal_span_x, view_angle_x, delta_d);
+fpy = frustum_projection(cal_span_y, view_angle_y, delta_d);
+pp = [ normalized_reference_points(:, 1)' .* fpx; ...
+       normalized_reference_points(:, 2)' .* fpy ]';
+if nargin > 7
   if do_plot
     plot(normalized_reference_points(:, 1), ...
          normalized_reference_points(:, 2), 'x', ...
