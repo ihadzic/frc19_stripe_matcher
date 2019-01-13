f042f06a1983ac45c33e69f420a1c4616fc169e4
Ilija Hadzic <ilijahadzic@gmail.com>
2019-01-13 17:14:25 -0500

    add docs
    Document the top level Matlab file, so that it shows
    in 'help' command and add a short readme that pretty-much
    refers to the Matlab help.
    
    Signed-off-by: Ilija Hadzic <ilijahadzic@gmail.com>

---
+++ README.txt
@@ -0,0 +1,9 @@
+Matlab model of the position estimation algorithm for FRC Deep Space
+2019 game using reflective-stripe guides placed at the walls
+of the rocket, cargo ship, and loading area.
+
+Quick example:
+
+[x, y, d]  = sim_estimation(1, 1.542, 0.792, 55, 77, 1, 0.1, 2.5, 0.01, 0.05, false, true)
+
+Type 'help sim_estimation' (in Matlab) for details.
--- sim_estimation.m
+++ sim_estimation.m
@@ -3,6 +3,74 @@ function [x, y, d] = sim_estimation(cal_dist, cal_span_x, cal_span_y, ...
                                     offset_x, offset_y, distance, ...
                                     bias, noise, ...
                                     use_meters, do_plot)
+%
+%function [x, y, d] = sim_estimation(cal_dist, cal_span_x, cal_span_y, ...
+%                                    view_angle_x, view_angle_y, ...
+%                                    offset_x, offset_y, distance, ...
+%                                    bias, noise, ...
+%                                    use_meters, do_plot)
+%
+% Simulates the position estimation algorithm for FRC Deep Space
+% 2019 game using reflective-stripe guides placed at the walls
+% of the rocket, cargo ship, and loading area. The assumption is
+% that before running this algorithm the four corner points
+% of each stripe have been identified and that their coordinates
+% have been determined. The identification of corner-points can be
+% done using a number of well known vision processing algorithms
+% (OpenCV gives a bunch of options that can be stitched together
+% with some "plumbing" code).
+%
+% This model will start from some assumed (ground truth) position
+% of the camera facing the reflective stripes. It will then
+% calculate what the camera would see and what corner points would
+% be identified by the vision processing. Next, it will add noise
+% and bias error. Observed points determined in this way are
+% then run through the estimator and the distance, and shift
+% (along X and Y axis) are recovered. The estimator works
+% by finding the scale and shift that minimizes the mean
+% square error between observed and reference points.
+%
+% In the absence of noise, the estimator will recover
+% the ground truth.
+%
+% If the noise or bias are present, the result will
+% have an error. Running the model multiple times for same
+% parameters will produce a set of results that can be used
+% to understand the statistical properties of the error
+%
+% The model requires that the camera properties (geometric
+% properties of the viewing frustum) are known, which can be
+% determined by calibration.
+%
+% Parameters
+%
+% cal_dist: distance at which the camera has been calibrated
+% cal_span_x: width of the area seen at calibration position
+% cal_span_y: height of the area seen at calibration position
+% view_angle_x: camera view angle along X axis
+% view_angle_y: camera view angle along Y axis
+% offset_x: ground truth location of the camera along X axis
+% offset_y: ground truth location of the camera along Y axis
+% distance: ground truth camera distance from the pattern
+% bias: systematic error (bias) stddev (Gaussian process assumed)
+% noise: noise stddev (Gaussian process assumed)
+% use_meters: set to true to use metric units (meters)
+%             otherwise imperial units are assumed (feet)
+% do_plot: set to true to show the plot of the reference points
+%          and observed points
+%
+% Returns:
+%
+% Estimates of x, y position and distance to the marker.
+%
+% Limitations:
+%
+% The model assumes that the camera is facing the
+% marker directly at 90-degree angle. If the pattern is viewed
+% from some other angle, an estimation error will be introduced.
+% It should not be too hard to add the angular offset to the model.
+%
+
 if nargin <= 10
   use_meters = false
 end
@@ -24,6 +92,7 @@ Ay = [ normalized_reference_points(:, 2)'; ...
        -ones(1, length(normalized_reference_points(:, 2)))]';
 soltn_x = (Ax' * Ax)^-1*Ax' * observed_points(:, 1);
 soltn_y = (Ay' * Ay)^-1*Ay' * observed_points(:, 2);
+
 % work backwards to get the distance and offset
 sigma_dx = soltn_x(1);
 sigma_dy = soltn_y(1);
