2da141c111bd86fb9fc7343e6be01ed287d73d59
Ilija Hadzic <ilijahadzic@gmail.com>
2019-01-13 12:28:32 -0500

    add a missing semicolon
    Signed-off-by: Ilija Hadzic <ilijahadzic@gmail.com>

--- frustum_projection.m
+++ frustum_projection.m
@@ -1,7 +1,7 @@
 function p=frustum_projection(cal_span, view_angle, delta_d, do_plot)
 
 tt = tan(deg2rad(view_angle / 2));
-p = cal_span ./ (cal_span + 2 * delta_d * tt)
+p = cal_span ./ (cal_span + 2 * delta_d * tt);
 if nargin > 3
   if do_plot
     plot(delta_d, p)
